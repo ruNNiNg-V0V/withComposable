ebd67b5e330bdc7700b087ff841f43b5e15619b4
천경훈 <grapduck@gmail.com>
2025-01-07 14:26:29 +0900

    첫 예시화면
    컴포저블로 나타내기

--- app/src/main/java/com/example/withcomposable/MainActivity.kt
+++ app/src/main/java/com/example/withcomposable/MainActivity.kt
@@ -4,16 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Constraints
-import androidx.constraintlayout.compose.ConstrainedLayoutReference
-import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.withcomposable.ui.theme.WithComposableTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,26 +23,35 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WithComposableTheme {
-                Greeting("Android")
+                // 리니어 레이아웃
+                Column(modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center)
+                {
+                    theText("Lake Louise")
+                    theImage("lake_1")
+                    theText("Lake Louise, AB, 캐나다")
+                }
             }
         }
     }
 }
 
 @Composable
-fun Greeting(s: String, modifier: Modifier = Modifier) {
-    ConstraintLayout(modifier = modifier.fillMaxSize()) {
-        val (text) = createRefs()
-        Text(
-            text = "Hello $s!",
-            modifier = Modifier.constrainAs(text) {
-                top.linkTo(parent.top)
-                bottom.linkTo(parent.bottom)
-                start.linkTo(parent.start)
-                end.linkTo(parent.end)
-            }
-        )
-    }
+fun theText(s: String) {
+    Text(
+        text = "$s"
+    )
+}
+
+@Composable
+fun theImage(src : String){
+    val context = LocalContext.current
+    val resourceId = context.resources.getIdentifier(src, "drawable", context.packageName)
+    Image(
+        painter = painterResource(id = resourceId),
+        contentDescription = src
+    )
 }
 
 // 미리보기
@@ -49,6 +59,16 @@ fun Greeting(s: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     WithComposableTheme {
-        Greeting("Android")
+        WithComposableTheme {
+            // 리니어 레이아웃
+            Column(modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center)
+            {
+                theText("Lake Louise")
+                theImage("lake_1")
+                theText("Lake Louise, AB, 캐나다")
+            }
+        }
     }
 }
